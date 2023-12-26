71d14a7e53d5f4ebed62b5560f0981b6539bf574
John Whitley <john@luminous-studios.com>
2023-12-26 08:43:57 -0800

    Fix nvim-tmux-navigation settings
    Also remove <C-Space> binding as I don't use it, and it conflicts with
    a default nvim-cmp binding.

--- .config/nvim/lua/plugins/tmux.lua
+++ .config/nvim/lua/plugins/tmux.lua
@@ -2,19 +2,16 @@ return {
   {
     "alexghergh/nvim-tmux-navigation",
     event = "VeryLazy",
-    config = function()
-      require("nvim-tmux-navigation").setup({
-        disable_when_zoomed = true, -- defaults to false
-        -- TODO: why doesn't 'keybindings' work?
-        -- keybindings = {
-        --   left = "<C-h>",
-        --   down = "<C-j>",
-        --   up = "<C-k>",
-        --   right = "<C-l>",
-        --   last_active = "<C-\\>",
-        --   next = "<C-Space>",
-        -- },
-      })
-    end,
+    opts = {
+      disable_when_zoomed = true, -- defaults to false
+      keybindings = {
+        left = "<C-h>",
+        down = "<C-j>",
+        up = "<C-k>",
+        right = "<C-l>",
+        last_active = "<C-\\>",
+        -- next = "<C-Space>",
+      },
+    },
   },
 }
