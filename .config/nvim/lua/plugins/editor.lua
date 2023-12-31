a6163a3ab92db7854738c82222d9a2a224ec51f0
John Whitley <john@luminous-studios.com>
2023-12-30 20:01:06 -0800

    Add nvim-treesitter incremental selection config

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -74,6 +74,13 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          node_incremental = ".",
+          node_decremental = ",",
+        },
+      },
       textobjects = {
         select = {
           keymaps = {
