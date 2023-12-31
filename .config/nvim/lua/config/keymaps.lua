b7a55a70c760f0a54f1c44e585cf703df464889d
John Whitley <john@luminous-studios.com>
2023-12-31 12:10:53 -0800

    Switch keymaps to use which-key
    Also adds a binding for the new :VcshShow command

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,6 +1,18 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local wk = require("which-key")
+
+wk.register({
+  ["<space>"] = { require("mux").mux, "Find files(mux)" },
+  n = { require("nvim-navbuddy").open, "Open Navbuddy" },
+  v = {
+    name = "vcsh",
+    e = { require("vcsh").vcshEnterSelect, "Enter a vcsh repo" },
+    s = { require("vcsh").vcshShow, "Show current vcsh repo" },
+    x = { require("vcsh").vcshExit, "Exit vcsh repo" },
+  },
+}, { prefix = "<leader>" })
 
 vim.keymap.set("n", "<C-h>", "<Cmd>NvimTmuxNavigateLeft<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-j>", "<Cmd>NvimTmuxNavigateDown<CR>", { noremap = true, silent = true })
@@ -8,9 +20,3 @@ vim.keymap.set("n", "<C-k>", "<Cmd>NvimTmuxNavigateUp<CR>", { noremap = true, si
 vim.keymap.set("n", "<C-l>", "<Cmd>NvimTmuxNavigateRight<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-\\>", "<Cmd>NvimTmuxNavigateLastActive<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-Space>", "<Cmd>NvimTmuxNavigateNavigateNext<CR>", { noremap = true, silent = true })
-
-vim.keymap.set("n", "<leader><space>", require("mux").mux, { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>n", require("nvim-navbuddy").open, { desc = "Open Navbuddy", noremap = true, silent = true })
-
-vim.keymap.set("n", "<leader>ve", require("vcsh").vcshEnterSelect, { desc = "Enter a vcsh repo", noremap = true, silent = true })
-vim.keymap.set("n", "<leader>vx", require("vcsh").vcshExit, { desc = "Exit vcsh repo", noremap = true, silent = true })
