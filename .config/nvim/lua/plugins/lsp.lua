a1d1fa7641fc687fd6715f6cdccc260bc4706ea1
John Whitley <john@luminous-studios.com>
2023-12-31 12:09:32 -0800

    Increase the LuaLS max_line_length
    This was somehow set far too short, causing LuaLS to be overly aggressive
    about multi-line reformatting. This is particularly annoying in neovim
    config files.

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -21,6 +21,19 @@ return {
     },
     opts = {
       servers = {
+        lua_ls = {
+          settings = {
+            Lua = {
+              format = {
+                enable = true,
+                -- format reference: https://github.com/CppCXY/EmmyLuaCodeStyle/blob/master/docs/format_config_EN.md
+                defaultConfig = {
+                  max_line_length = 120,
+                },
+              },
+            },
+          },
+        },
         sourcekit = {},
       },
     },
