e818ae67c445ece00bcb695dfaa8f4d95ba4173a
John Whitley <john@luminous-studios.com>
2024-03-21 10:00:09 -0700

    Add ensure_installed stanza to nvim-treesitter config
    This change also refactors treesitter configs into their own file

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -54,42 +54,6 @@ return {
     enabled = false,
   },
   {
-    "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-      "RRethy/nvim-treesitter-textsubjects",
-      config = function()
-        require("nvim-treesitter.configs").setup({
-          textsubjects = {
-            enable = true,
-            prev_selection = ",", -- (Optional) keymap to select the previous selection
-            keymaps = {
-              ["."] = "textsubjects-smart",
-              [";"] = "textsubjects-container-outer",
-              ["i;"] = { "textsubjects-container-inner", desc = "Select inside containers (classes, functions, etc.)" },
-            },
-          },
-        })
-      end,
-    },
-    opts = {
-      -- incremental_selection = {
-      --   enable = true,
-      --   keymaps = {
-      --     node_incremental = ".",
-      --     node_decremental = ",",
-      --   },
-      -- },
-      textobjects = {
-        select = {
-          keymaps = {
-            ["ia"] = "@parameter.inner",
-            ["oa"] = "@parameter.outer",
-          },
-        },
-      },
-    },
-  },
-  {
     "jwhitley/vcsh.nvim",
   },
   {
